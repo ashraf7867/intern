DurandA/flask-restplus-server-example

Improve support for JSON parameters
When using api.parameters decorator, the locations parameter is now taken into account to generate the documentation ("@api.parameters(parameters.MyParameters(many=False), locations=('json',))".

flask_restplus_patched/namespace.py
@@ -70,6 +70,8 @@ class Namespace(OriginalNamespace):
                 _locations = ('json', )
             else:
                 _locations = locations
+            if locations is not None:
+                parameters.context['in'] = locations
 
             return self.doc(params=parameters)(
                 self.response(code=http_exceptions.UnprocessableEntity.code)(

ashraf7867/intern

flask_restplus_patched/namespace.py
@@ -70,6 +70,8 @@ class Namespace(OriginalNamespace):
                 _locations = ('json', )
             else:
                 _locations = locations
+            if _locations is not None:
+                parameters.context['in'] = _locations
 
             return self.doc(params=parameters)(
                 self.response(code=http_exceptions.UnprocessableEntity.code)(